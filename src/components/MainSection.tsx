032c2e84335badb8205679388a0bc46600c74805
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-01-17 22:25:40 +0100

    💄 Added main section component

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -1,5 +1,5 @@
 export function MainSection({children}:{children:React.ReactNode}) {
-    return <div className="h-full w-[60%] shadow-sm shadow-custom-resalt-color bg-[#111111] overflow-hidden rounded-3xl p-4 text-white backdrop-blur-md bg-transparent">
+    return <div className=" h-full w-[60%] shadow-sm border-1 border-custom-border-color bg-[#151515] overflow-hidden rounded-3xl p-2 text-white backdrop-blur-md">
         {children}
     </div>
 }

6a34f976d0eb93e53f794401aeb9ca273eda99c1
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-01-20 23:45:21 +0100

    👽️ Added ChatRooms to UserResponse.

--- src/services/user/mapper.ts
+++ src/services/user/mapper.ts
@@ -4,6 +4,11 @@ export const mapUserResponseToUser = (userResponse:UserResponse):User => {
     return {
         email: userResponse.email,
         online: userResponse.online,
-        username: userResponse.username
+        username: userResponse.username,
+        chatRooms: userResponse.chatRooms.length > 0 ? userResponse.chatRooms.map((chatRoom) =>  ({
+            code: chatRoom.code,
+            messages: [],
+            name: chatRoom.name
+        })) : []
     }
 }

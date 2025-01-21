26fc074a8ce8a592984411e96d26d86711c76de3
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-01-21 22:04:25 +0100

    💄 UI Fix for Chat section.

--- src/pages/chat/components/rooms/RoomButton.tsx
+++ src/pages/chat/components/rooms/RoomButton.tsx
@@ -9,7 +9,7 @@ export function RoomButton({
     <div className="flex gap-2 w-full">
       <button
         onClick={() => showCreateRoomModal(true)}
-        className="group bg-custom-border-color px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out hover:gap-2"
+        className="group bg-[#151515] shadow-xl px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out hover:gap-2"
         >
         <LiaPlusSolid className="transition-transform duration-300 ease-in-out group-hover:scale-110" />
         <span className="opacity-0 group-hover:opacity-100 max-w-0 group-hover:max-w-xs overflow-hidden transition-all duration-200 ease-in-out">
@@ -18,7 +18,7 @@ export function RoomButton({
     </button>
       <button
         onClick={() => showCreateRoomModal(true)}
-        className="bg-custom-border-color px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center gap-2 rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out"
+        className="bg-[#151515] shadow-xl px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center gap-2 rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out"
       >
         <IoEnterOutline />
       </button>

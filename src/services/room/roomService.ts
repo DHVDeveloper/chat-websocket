dd1deccdc8fc0c99e277e8733b0bd4bed85357da
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-09 13:36:06 +0100

    ✨ Added get room service.

--- src/services/room/roomService.ts
+++ src/services/room/roomService.ts
@@ -1,3 +1,6 @@
+import { ChatRoom } from "@/domain/interfaces/room/chatRoom";
+import { roomEndpoint } from "./roomEndpoint";
+
 export const roomService = {
     create: async (name: string): Promise<any> => {
         try {
@@ -21,4 +24,40 @@ export const roomService = {
             throw error;
         }
     },
+    join: async (roomCode:string, email:string): Promise<any> => {
+        try {
+            const response = await fetch('/api/chatRoom', {
+                method: 'PATCH',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ roomCode , email}),
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error al unirse a la sala: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error al unirse a la sala:', error);
+            throw error;
+        }  
+    },
+    getRoomData: async (roomCode:string): Promise<ChatRoom> => {
+        try{
+            const response = await fetch(roomEndpoint.getRoom(roomCode));
+
+            if (!response.ok) {
+                throw new Error(`Error al recuperar la sala: ${response.statusText}`);
+            }
+
+            const data = await response.json();
+            return data as ChatRoom;
+        } catch {
+            throw new Error("Error al recuperar la sala");
+        }
+    }
 };

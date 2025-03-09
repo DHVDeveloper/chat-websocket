18f55614b79ad5c5e4eaa98d2864d34a1c65ade6
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-09 13:25:01 +0100

    👽️ Added get chatroom.

---
+++ src/app/api/chatRoom/[code]/route.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server';
+import ChatRoom from '@/models/ChatRoom';
+import { connectMongo } from '@/utils/connectMongo';
+import Message from '@/models/Message';
+import User from '@/models/User';
+
+export const GET = async ({ params }: { params: { code: string } }) => {
+  const code = params.code
+  if (!code) {
+    return new NextResponse(JSON.stringify({ error: 'El código de sala es obligatorio' }), { status: 400 });
+  }
+
+  try {
+    await connectMongo();
+
+    const room = await ChatRoom.findOne({ code: { $in: code } }).populate({
+      path: 'messages',
+      model: Message,
+      populate: [
+        { path: 'from', model: User },
+        { path: 'to', model: User },
+      ],
+    });
+
+    if (!room) {
+      return new NextResponse(JSON.stringify({ error: 'No se han encontrado salas con los códigos proporcionados' }), { status: 404 });
+    }
+
+    return new NextResponse(JSON.stringify(room), { status: 200 });
+  } catch (error) {
+    
+    return new NextResponse(JSON.stringify({ error: 'Error al procesar la petición' }), { status: 500 });
+  }
+};

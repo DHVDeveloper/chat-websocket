60d9798606d8f0bde27486ca67c4b2430640a4e5
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-09 15:12:02 +0100

    🐛 Fix register redirect.

--- src/pages/register/Register.tsx
+++ src/pages/register/Register.tsx
@@ -1,8 +1,10 @@
 'use client'
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
 
 export function RegisterPage() {
+  const router = useRouter()
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -33,6 +35,7 @@ export function RegisterPage() {
       return
     }
     toast.success("Se ha registrado correctamente sesión correctamente!")
+    router.push('chat')
   };
   return (
     <section className="flex items-center justify-center w-full h-full rounded-3xl bg-custom-tertiary-color border-[1px] border-custom-border-color">

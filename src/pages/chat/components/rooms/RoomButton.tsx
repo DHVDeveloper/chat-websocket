edefac930ac9f8dfe2db1d2f7c11dffa99c504f4
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-09 13:08:29 +0100

    💄 UI Fix to Room Section.

--- src/pages/chat/components/rooms/RoomButton.tsx
+++ src/pages/chat/components/rooms/RoomButton.tsx
@@ -1,26 +1,34 @@
 import { LiaPlusSolid } from "react-icons/lia";
 import { IoEnterOutline } from "react-icons/io5";
+import { BiPlusCircle } from "react-icons/bi";
+import { HiPlus } from "react-icons/hi";
+import { RiChatForwardFill } from "react-icons/ri";
 export function RoomButton({
   showCreateRoomModal,
+  showJoinRoomModal
 }: {
   showCreateRoomModal: (show: boolean) => void;
+  showJoinRoomModal: (show:boolean) => void
 }) {
   return (
     <div className="flex gap-2 w-full">
       <button
         onClick={() => showCreateRoomModal(true)}
-        className="group bg-[#151515] shadow-xl px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out hover:gap-2"
+        className="group bg-[#ffffff10] shadow-md px-2 py-2 text-white border-1  flex items-center justify-center rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out hover:gap-2"
         >
-        <LiaPlusSolid className="transition-transform duration-300 ease-in-out group-hover:scale-110" />
+        <HiPlus className="transition-transform duration-300 ease-in-out group-hover:scale-110" />
         <span className="opacity-0 group-hover:opacity-100 max-w-0 group-hover:max-w-xs overflow-hidden transition-all duration-200 ease-in-out">
             Crear sala
         </span>
     </button>
       <button
-        onClick={() => showCreateRoomModal(true)}
-        className="bg-[#151515] shadow-xl px-2 py-2 text-custom-resalt-color border-1 border-custom-border-color flex items-center justify-center gap-2 rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out"
+        onClick={() => showJoinRoomModal(true)}
+        className="bg-[#ffffff10] shadow-xl p-2 border-1 flex items-center justify-center gap-2 rounded-md text-nowrap hover:text-custom-secondary-color hover:bg-custom-resalt-color transition-all duration-200 ease-in-out"
       >
-        <IoEnterOutline />
+        <RiChatForwardFill className="transition-transform duration-300 ease-in-out group-hover:scale-110" />
+        <span className="opacity-0 group-hover:opacity-100 max-w-0 group-hover:max-w-xs overflow-hidden transition-all duration-200 ease-in-out">
+            Unirse a una sala
+        </span>
       </button>
     </div>
   );
--- src/pages/chat/components/rooms/RoomList.tsx
+++ src/pages/chat/components/rooms/RoomList.tsx
@@ -1,15 +1,15 @@
 import { useUserContext } from "@/context/user/User.context"
 import { IoChatbubblesSharp } from "react-icons/io5";
 
-export function RoomList(){
+export function RoomList({handleSelectChat}:{handleSelectChat: (roomCodeSelected:string) => void}){
     const {user: {chatRooms}} = useUserContext()
     const {user} = useUserContext()
     return <div className="w-full h-full p-3 gap-3 flex flex-col pt-1 overflow-y-auto">
             {chatRooms && chatRooms.length > 0 && chatRooms.map((chatRoom) => (
-                <div key={chatRoom.code} className="w-full rounded-md transition-all cursor-pointer bg-[#151515] shadow-xl  ps-3 py-4 flex gap-2 hover:bg-[#1b1b1b]">
-                    <span className="bg-[#0cfdc542] rounded-full text-custom-resalt-color flex justify-center items-center px-2"><IoChatbubblesSharp /></span>
+                <button onClick={() => handleSelectChat(chatRoom.code)} key={chatRoom.code} className="w-full rounded-lg transition-all cursor-pointer shadow-md bg-[#151515] ps-3 py-4 flex gap-2 hover:bg-[#1b1b1b]">
+                    <span className="bg-[#ffffff10] p-2 rounded-lg shadow-lg flex justify-center items-center px-2"><IoChatbubblesSharp /></span>
                     {chatRoom.name}
-                </div>
+                </button>
             ))}
     </div>
 }

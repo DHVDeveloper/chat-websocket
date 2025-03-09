31c9f7cd066e7e920f21dece19a95998dc4bdd7f
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-09 16:40:31 +0100

    💄 Added new colors to skeleton.

--- src/pages/chat/sections/side/chats/rooms/RoomSkeleton.tsx
+++ src/pages/chat/sections/side/chats/rooms/RoomSkeleton.tsx
@@ -3,10 +3,10 @@ export function RoomSkeleton(){
     {Array.from({ length: 10 }).map((_, index) => (
       <div
         key={index}
-        className="w-full rounded-lg flex items-center shadow-md bg-black/25 ps-3 py-4  gap-2 animate-pulse"
+        className="w-full rounded-lg flex items-center shadow-md bg-[#ffffff10] ps-3 py-4  gap-2 animate-pulse"
       >
-        <div className="w-10 h-10 bg-gray-700 rounded-md"></div>
-          <div className="h-4 bg-gray-700 rounded w-3/4 mb-2"></div>
+        <div className="w-10 h-10 bg-[#7e7e7e10] rounded-md"></div>
+          <div className="h-4 bg-[#7e7e7e10] rounded w-3/4 mb-2"></div>
       </div>
     ))}
   </div>
--- src/pages/chat/sections/side/user/UserSection.skeleton.tsx
+++ src/pages/chat/sections/side/user/UserSection.skeleton.tsx
@@ -1,11 +1,11 @@
 export function UserSectionSkeleton(){
     return (<div
-        className="w-full rounded-xl flex items-center justify-between shadow-md bg-black/25 px-3 py-5  gap-2 animate-pulse"
+        className="w-full rounded-xl flex items-center justify-between shadow-md bg-[#ffffff10] px-3 py-5  gap-2 animate-pulse"
       >
         <div className="flex flex-1 gap-2 items-center">
-            <div className="w-6 h-6 bg-gray-700 rounded-md"></div>
-            <div className="h-4 bg-gray-700 rounded w-3/4 mb-2"></div>
+            <div className="w-6 h-6 bg-bg-[#7e7e7e10] rounded-md"></div>
+            <div className="h-4 bg-bg-[#7e7e7e10] rounded w-3/4 mb-2"></div>
         </div>
-        <div className="w-6 h-6 bg-gray-700 rounded-md"></div>
+        <div className="w-6 h-6 bg-bg-[#7e7e7e10] rounded-md"></div>
       </div>)
 }

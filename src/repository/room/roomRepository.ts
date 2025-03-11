95587e95893c06c3d8559aabcb151318825e4c55
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-11 21:41:57 +0100

    ✨ Added room repository.

---
+++ src/repository/room/roomRepository.ts
@@ -0,0 +1,41 @@
+import { ApiResponse } from "@/types/apiResponse";
+import { apiFetch } from "@/utils/apiFetch";
+import { ChatRoomResponse } from "./chatRoomModel";
+import { chatRoomRoutes } from "./chatRoomRoutes";
+
+export const chatRoomRepository = {
+  create: async (name: string): Promise<ApiResponse<ChatRoomResponse>> => {
+    const response = await apiFetch<ChatRoomResponse>(chatRoomRoutes.create(), {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name }),
+    });
+
+    return response;
+  },
+
+  join: async (roomCode: string, email: string): Promise<ApiResponse<ChatRoomResponse>> => {
+    const response = await apiFetch<ChatRoomResponse>(chatRoomRoutes.join(), {
+      method: 'PATCH',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ roomCode, email }),
+    });
+
+    return response;
+  },
+
+  getRoomData: async (roomCode: string): Promise<ApiResponse<ChatRoomResponse>> => {
+    const response = await apiFetch<ChatRoomResponse>(chatRoomRoutes.getRoom(roomCode), {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    return response;
+  },
+};

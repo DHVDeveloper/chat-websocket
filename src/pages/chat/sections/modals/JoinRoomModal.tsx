569ea22f3b76310c38b8982858c7cc470f931bc0
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-13 21:57:49 +0100

    ✨ Added  form controls in Join Room.

--- src/pages/chat/sections/modals/JoinRoomModal.tsx
+++ src/pages/chat/sections/modals/JoinRoomModal.tsx
@@ -1,6 +1,6 @@
 import { useUserContext } from "@/context/user/User.context";
 import { chatRoomService } from "@/services/chatRoomService";
-import { FormEvent, useState } from "react";
+import { FormEvent, useRef, useState } from "react";
 import { toast } from "sonner";
 
 interface JoinModalProps {
@@ -10,22 +10,30 @@ interface JoinModalProps {
 
 export function JoinRoomModal({ isOpen, handleIsOpenModal }: JoinModalProps) {
   const [roomCode, setRoomCode] = useState("");
-  const {user: {email}} = useUserContext()
+  const {user: {email},reloadChatRooms} = useUserContext()
+    const formRef = useRef<HTMLFormElement>(null);
 
-  const joinRoom = async () => {
+  const joinRoom = async (e: React.FormEvent) => {
+    e.preventDefault(); 
+    if (formRef.current && !formRef.current.checkValidity()) {
+      formRef.current.reportValidity();
+      return;
+    }
     const joinRoomResponse = await chatRoomService.join(roomCode,email);
     if(!joinRoomResponse.success) {
       return toast.error(joinRoomResponse.error)
     }
     handleIsOpenModal(false)
     toast.success("Se ha creado la sala correctamente!")
+    reloadChatRooms()
   };
 
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
       <div className="bg-custom-main-color rounded-lg shadow-lg max-w-md w-full p-6">
-        <h2 className="text-xl font-semibold text-white">Unirme a sala</h2>
+        <h2 className="text-xl font-semibold text-white pb-3">Unirme a sala</h2>
 
+        <form action="" ref={formRef}>
         <p>
           <input
             type="text"
@@ -44,12 +52,14 @@ export function JoinRoomModal({ isOpen, handleIsOpenModal }: JoinModalProps) {
             Cerrar
           </button>
           <button
+            type="submit"
             onClick={joinRoom}
-            className="px-4 py-2 text-sm text-custom-main-color bg-custom-resalt-color rounded-md hover:bg-blue-600 transition-all"
+            className="px-4 py-2 text-sm bg-[#0cfdc625] text-custom-resalt-color rounded-md hover:bg-[#0cfdc655] transition-all"
           >
             Confirmar
           </button>
         </div>
+        </form>
       </div>
     </div>
   );

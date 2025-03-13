db713e63847addab6da9f46c9775f481435cee16
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-13 22:00:15 +0100

    👽️ Added getChatRoomsByUser endpoint.

--- src/repository/user/userRepository.ts
+++ src/repository/user/userRepository.ts
@@ -3,10 +3,19 @@ import { ApiResponse } from '@/types/apiResponse';
 import { UserResponse } from './userModels';
 import { apiFetch } from '@/utils/apiFetch';
 import { userRoutes } from './userRoutes';
+import { ChatRoom } from '@/domain/room/chatRoom';
 
 export const userRepository = {
   getUser: async (): Promise<ApiResponse<UserResponse>> => {
     const response = await apiFetch<UserResponse>(userRoutes.get(), {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    return response;
+  },
+  getChatRoomsByUser: async (): Promise<ApiResponse<ChatRoom[]>> => {
+    const response = await apiFetch<ChatRoom[]>(userRoutes.getRoomsByUser(), {
       method: 'GET',
       credentials: 'include',
     });
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,10 +3,20 @@ import { ApiResponse } from '@/types/apiResponse';
 import { User } from '@/domain/user/user';
 import { userRepository } from '@/repository/user/userRepository';
 import { mapUserResponseToUser } from '@/repository/user/userMappers';
+import { ChatRoom } from '@/domain/room/chatRoom';
 
 export const userService = {
   getUser: async (): Promise<ApiResponse<User>> => {
     const response = await userRepository.getUser();
+
+    if (!response.success) {
+      return { success: false, error: response.error || USER_ERROR_MESSAGES.USER_NOT_FOUND };
+    }
+
+    return { success: true, data: response.data  };
+  },
+  getChatRoomsByUser: async (): Promise<ApiResponse<ChatRoom[]>> => {
+    const response = await userRepository.getChatRoomsByUser();
 
     if (!response.success) {
       return { success: false, error: response.error || USER_ERROR_MESSAGES.USER_NOT_FOUND };

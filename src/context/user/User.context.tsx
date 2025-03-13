3747c0926e248cfb6d731d76d91a1a7911db25ff
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-13 21:58:52 +0100

    ✨ Added reload user chatrooms.

--- src/context/user/User.context.tsx
+++ src/context/user/User.context.tsx
@@ -7,6 +7,7 @@ import { createContext, useContext } from "react";
 interface UserContext{
     user:User
     isLoading: boolean
+    reloadChatRooms: () => void
 }
 
 export const UserContext = createContext<UserContext|undefined>(undefined)
--- src/context/user/User.provider.tsx
+++ src/context/user/User.provider.tsx
@@ -3,11 +3,12 @@ import { User } from "@/domain/user/user";
 import { userService } from "@/services/userService";
 import { useEffect, useState } from "react";
 import { UserContext } from "./User.context";
+import { toast } from "sonner";
 
 export function UserProvider({ children }: { children: React.ReactNode }) {
   const [user, setUser] = useState<User>({} as User);
   const [isLoading, setIsLoading] = useState<boolean>(true)
-
+  
   useEffect(() => {
       try {
         (async () => {
@@ -21,9 +22,16 @@ export function UserProvider({ children }: { children: React.ReactNode }) {
       }
   }, []); 
 
-
+  const reloadChatRooms = async () => {
+    const rooms = await userService.getChatRoomsByUser()
+    if(!rooms.data){
+      toast.error(rooms.error)
+      return
+    }
+    setUser({...user, chatRooms: rooms.data})
+  }
   return (
-    <UserContext.Provider value={{user,isLoading}}>
+    <UserContext.Provider value={{user,isLoading,reloadChatRooms}}>
       {children}
     </UserContext.Provider>
   );

2e5838b6bf93a8a232d29b4d6f6566e05dce1fd6
DHVDeveloper <danielhernandezvieiradvlp@gmail.com>
2025-03-17 22:47:53 +0100

    💄 UI Fix to message container.

--- src/pages/chat/sections/current-chat/chat/components/SendedMessage.tsx
+++ src/pages/chat/sections/current-chat/chat/components/SendedMessage.tsx
@@ -4,11 +4,11 @@ interface SendedMessage {
 }
 export function SendedMessage({ children, lastMessageSent }: SendedMessage) {
   return (
-    <div className="text-custom-text-color w-full flex justify-end items-center">
+    <div className="text-custom-text-color max-w-[100%] flex justify-end items-center">
       <div
-        className={`bg-custom-secondary-color  bg-opacity-40 backdrop-filter backdrop-blur-lg text-custom-text-color ${
+        className={`border shadow-lg bg-[#278b5b] w-fit max-w-[50%] backdrop-filter overflow-hidden backdrop-blur-lg text-custom-text-color ${
           lastMessageSent ? "rounded-lg rounded-br-none" : "rounded-lg"
-        } p-4 border-[1px] border-custom-border-color`}
+        } p-3 border-[1px] border-[#3f3f4680]`}
       >
         {children}
       </div>
